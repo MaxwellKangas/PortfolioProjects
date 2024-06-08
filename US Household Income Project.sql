91b9434081e11960e6457f2dcd677764b14458e3
Maxwell Kangas <mkangas18@gmail.com>
2024-06-08 15:18:07 -0400

    Create US Household Income Project.sql

---
+++ US Household Income Project.sql
@@ -0,0 +1,170 @@
+# US Household Income Data Cleaning
+
+SELECT * 
+FROM us_project.us_household_income;
+
+SELECT * 
+FROM us_project.us_household_income_statistics;
+
+ALTER TABLE us_project.us_household_income_statistics RENAME COLUMN `ï»¿id` TO `id`;
+
+SELECT COUNT(id) 
+FROM us_project.us_household_income;
+
+SELECT COUNT(id) 
+FROM us_project.us_household_income_statistics;
+
+SELECT id, COUNT(id)
+FROM us_project.us_household_income
+GROUP BY id
+HAVING COUNT(id) > 1 
+;
+
+SELECT *
+FROM (
+SELECT row_id,
+id,
+ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+FROM us_project.us_household_income
+) duplicates
+WHERE row_num > 1
+;
+
+
+
+DELETE FROM us_household_income
+WHERE row_id IN (
+	SELECT row_id
+	FROM (
+		SELECT row_id,
+		id,
+		ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+		FROM us_project.us_household_income
+		) duplicates
+	WHERE row_num > 1)
+;
+
+
+SELECT DISTINCT State_Name
+FROM us_project.us_household_income
+ORDER BY 1
+;
+
+
+UPDATE us_project.us_household_income
+SET State_Name = 'Georgia'
+WHERE State_Name = 'georia';
+
+UPDATE us_project.us_household_income
+SET State_Name = 'Alabama'
+WHERE State_Name = 'alabama';
+
+
+SELECT *
+FROM us_project.us_household_income
+WHERE County = 'Autauga County'
+ORDER BY 1
+;
+
+UPDATE us_household_income
+SET Place = 'Autaugaville'
+WHERE County = 'Autauga County'
+AND City = 'Vinemont' ;
+
+
+
+SELECT Type, COUNT(Type) 
+FROM us_project.us_household_income
+GROUP BY Type
+#ORDER BY 1
+;
+
+UPDATE us_household_income
+SET Type = 'Borough'
+WHERE Type = 'Boroughs'
+;
+
+SELECT ALand, AWater 
+FROM us_project.us_household_income
+WHERE (ALand = 0 OR ALand = '' OR ALand IS NULL)
+;
+
+
+# US Household Income Exploratory Data Analysis
+
+
+SELECT * 
+FROM us_project.us_household_income;
+
+SELECT * 
+FROM us_project.us_household_income_statistics;
+
+
+SELECT State_Name, SUM(ALand), SUM(AWater)
+FROM us_project.us_household_income
+GROUP BY State_Name
+ORDER BY 2 DESC
+LIMIT 10;
+
+SELECT State_Name, SUM(ALand), SUM(AWater)
+FROM us_project.us_household_income
+GROUP BY State_Name
+ORDER BY 3 DESC
+LIMIT 10;
+
+
+SELECT * 
+FROM us_project.us_household_income u
+JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id;
+
+
+SELECT * 
+FROM us_project.us_household_income u
+INNER JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id
+WHERE Mean <> 0;
+
+ 
+SELECT u.State_Name, County, Type, `Primary`, Mean, Median
+FROM us_project.us_household_income u
+INNER JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id
+WHERE Mean <> 0;
+
+SELECT u.State_Name, ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+FROM us_project.us_household_income u
+INNER JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id
+WHERE Mean <> 0
+GROUP BY u.State_Name
+ORDER BY 3 DESC
+LIMIT 10
+;
+
+
+
+SELECT Type, COUNT(Type), ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+FROM us_project.us_household_income u
+INNER JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id
+WHERE Mean <> 0
+GROUP BY 1
+HAVING COUNT(Type) > 100
+ORDER BY 4 DESC
+LIMIT 20
+;
+
+SELECT *
+FROM us_household_income
+WHERE Type = 'Community';
+
+
+SELECT u.State_Name, City, ROUND(AVG(Mean),1), ROUND(AVG(Median),1) 
+FROM us_project.us_household_income u
+JOIN us_project.us_household_income_statistics us
+	ON u.id = us.id
+GROUP BY u.State_Name, City
+ORDER BY ROUND(AVG(Mean),1) DESC;
+    
+    
